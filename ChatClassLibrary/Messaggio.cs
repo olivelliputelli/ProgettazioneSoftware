7c9d4088d782f552db6aea575b05f380a421bdfa
Olivelli Putelli <olivelliputelli@live.it>
2018-11-05 11:15:22 +0100

    Aggiunta classe Messaggio

---
+++ ChatClassLibrary/Messaggio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatClassLibrary
+{
+    class Messaggio
+    {
+        public Data DataMessaggio { get; set; }
+        public Orario OrarioMessaggio { get; set; }
+        public string TestoMessaggio { get; set; } = "";
+        public bool IsRicevuto { get; set; } = false;
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,6 +7,13 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            Orario o1 = new Orario();
+
+            o1.Ora = 12;
+            o1.Minuti = 0;
+            o1.Secondi = 0;
+
+
             Data data1 = new Data(12, 10, 2010);
             Data data2 = new Data(13, 10, 2010);
 
